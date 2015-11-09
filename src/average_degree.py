8234d1d47ac33ec43471f413c21cadb9801856ea
Alexander Fedoseev <alex@fedoseev.us>
2015-11-09 01:52:31 -0500

    Final Submission

--- src/average_degree.py
+++ src/average_degree.py
@@ -1,1 +1,70 @@
 # example of program that calculates the average degree of hashtags
+# To Do:
+# Extract hashtags and timestamp from each tweet
+
+
+import io
+import os
+import sys
+import json
+import codecs
+import string
+import networkx as nx #sudo pip install networkx
+import igraph as ig #sudo pip install igraph
+#import future  # requires pip install
+from os import path
+
+
+
+def main():
+
+  RAW_TWEETS_FILE = sys.argv[1]
+  AVERAGE_DEGREE_TWEETS_FILE = sys.argv[2]
+  tweets_with_hashtags = 0
+  tweetscount = 0
+  twitter_hashtag_list = []
+  
+  
+  hashtaglist = []
+
+  with io.open(RAW_TWEETS_FILE, encoding='utf-8') as f:
+    
+
+    print('\nStarting the second part of the challenge')
+    
+    G = nx.Graph()
+    gr = ig.Graph()  
+    
+    if path.isfile(AVERAGE_DEGREE_TWEETS_FILE):
+       try:
+          os.remove(AVERAGE_DEGREE_TWEETS_FILE)
+       except OSError:
+          pass
+    fout = open(AVERAGE_DEGREE_TWEETS_FILE,"a+",)
+
+    for line in f:
+      tweet = json.loads(line)    
+      
+      # Checking if that is  a valid tweet:
+      if 'text' in tweet:
+        tweetscount +=1 #Debugging
+      # Extractting data from the tweet
+        
+        hashtags = tweet["entities"]["hashtags"] # Creating a preliminary list of hashtags
+        
+        hashtaglist = []
+        for ht in hashtags:
+          if ht != None:
+            hashtaglist.append(ht["text"].encode("ascii", "ignore")) # Creating a list of valid tweets
+             
+        if len(hashtaglist) > 1:
+           
+           # Creating edges to insert into the graph
+           edges = [hashtaglist[i]+','+hashtaglist[i+1] for i in range(len(hashtaglist)-1)]
+           #G.add_edges_from[edges] 
+
+
+    fout.close()
+    
+if __name__ == "__main__":
+  main()
--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,74 @@
-# example of program that calculates the number of tweets cleaned
+# My implementation of the coding challenge
+# -*- coding: UTF-8 -*-
+# TO DO:
+# Identify Non ASCI codes in UNICODE string
+
+import io
+import os
+import sys
+import json
+import codecs
+import string
+
+#import future  # requires pip install
+from os import path
+
+def isUnicode(s): # 
+  # if encode and decode equals the original
+  # than it doesnt have special characters
+  encoded_text = s.encode("ascii", "ignore")
+  decoded_text = encoded_text.decode('utf-8')
+  if decoded_text == s:
+    return False
+  return True
+  
+if __name__ == "__main__":
+   
+
+  #0.1 input parameters
+  RAW_TWEETS_FILE = sys.argv[1]
+  CLEANED_TWEETS_FILE = sys.argv[2]
+  
+  with io.open(RAW_TWEETS_FILE,encoding='utf-8') as f:    
+    Unicode_Tweets_Count = 0;    
+    
+    raw_tweet_text = ""
+    created_at_text = ""
+    if path.isfile(CLEANED_TWEETS_FILE):
+       try:
+          os.remove(CLEANED_TWEETS_FILE)
+       except OSError:
+          pass
+      
+    fout = open(CLEANED_TWEETS_FILE,"a+",)
+    print('Starting the first part of the challenge')
+    
+    for line in f:
+      tweet = json.loads(line)
+      #1.1 Extract text from the tweet
+      
+      # Checking if that is  a valid tweet:
+      if 'text' in tweet:
+          
+        raw_tweet_text = tweet['text']
+        raw_tweet_text = raw_tweet_text.replace('\n', ' ')
+        raw_tweet_text = raw_tweet_text.replace('\t', ' ')
+
+        encoded_tweet_text = raw_tweet_text.encode("ascii", "ignore")
+        no_white_space_tweet = encoded_tweet_text.strip()
+
+        if  isUnicode(raw_tweet_text):
+            Unicode_Tweets_Count +=1
+        
+      #1.3 Extract timestamp: 
+        timestamp_text = " (timestamp: " + tweet['created_at']  + ")"
+       
+        fout.write(no_white_space_tweet + timestamp_text + "\n")
+        
+  # close the file when done
+  fout.write('\n' + str(Unicode_Tweets_Count) + " tweets contained unicode")
+  fout.close()
+  
+  #print "the file had " + str(linecount) + " tweets" # DEBUG LINE
+  #print str(Unicode_Tweets_Count) + " tweets contained unicode" # DEBUG LINE
+  
